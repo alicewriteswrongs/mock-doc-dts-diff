1df353e9338b424952b67c63e5474630059740c8
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-03 18:09:09 -0400

    comparison script

---
+++ compare.js
@@ -0,0 +1,34 @@
+const typescript = require("typescript");
+
+const createProgramForFile = (filename) => {
+  const program = typescript.createProgram([filename], {});
+  return program;
+};
+
+const exportedSymbolsFromFile = (filename) => {
+  const program = createProgramForFile(filename);
+  const sourceFile = program.getSourceFile(filename);
+  const checker = program.getTypeChecker();
+  const fileSymbol = checker.getSymbolAtLocation(sourceFile);
+  const out = [];
+  if (fileSymbol && fileSymbol.exports) {
+    fileSymbol.exports.forEach((symbol, name) => {
+      out.push(name);
+    });
+    return out;
+  }
+};
+
+const esbuildSymbols = exportedSymbolsFromFile("./esbuild-index.d.ts");
+const rollupSymbols = exportedSymbolsFromFile("./rollup-index.d.ts");
+
+const allSymbols = new Set([...esbuildSymbols, ...rollupSymbols]);
+
+console.log("CHECKING EXPORTED SYMBOLS");
+for (const symbol of allSymbols) {
+  const rollup = rollupSymbols.includes(symbol) ? "✅" : "❌";
+  const esbuild = esbuildSymbols.includes(symbol) ? "✅" : "❌";
+  const space =
+    symbol.length < 15 ? "\t\t\t" : symbol.length < 20 ? "\t\t" : "\t";
+  console.log(`${symbol} ${space}rollup: ${rollup}\tesbuild: ${esbuild}`);
+}
